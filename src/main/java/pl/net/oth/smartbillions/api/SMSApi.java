3fac7a2dfad5f793e916cb0e46bedc137057ea4e
psokolowski <piotr.sokolowski@gmail.com>
2018-01-22 10:18:03 +0100

    Poprawiono wysyłanie SMS

--- src/main/java/pl/net/oth/smartbillions/api/SMSApi.java
+++ src/main/java/pl/net/oth/smartbillions/api/SMSApi.java
@@ -1,5 +1,6 @@
 package pl.net.oth.smartbillions.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -10,13 +11,17 @@ import pl.net.oth.smartbillions.model.SMSMessage;
 
 @Component
 public class SMSApi {
+	@Autowired
+	private DatabaseAPI databaseAPI;
+	
 	public void sendSMS(String message) {
 		System.out.println("Wysyłka SMS");
+		String address=databaseAPI.getConfigurationValue("SMS_API");
 		SMSMessage smsMessage=new SMSMessage();
 		smsMessage.setText(message);
 		smsMessage.setPhoneNumber(App.PHONE_NUMBER);
 		HttpEntity<SMSMessage> request = new HttpEntity<>(smsMessage);
 		RestTemplate restTemplate = new RestTemplate();		
-		Boolean result = restTemplate.postForObject("http://zjc.oth.net.pl/sendSMS",request, Boolean.class);     	
+		Boolean result = restTemplate.postForObject("http://"+address+"/sendSMS",request, Boolean.class);     	
 	}
 }
